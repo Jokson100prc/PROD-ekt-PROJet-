1d6527a92e15a1551e55a575e8f53a1442e9cb75
Marcin Zawacki <marcinzacki@gmail.com>
2018-10-11 14:36:54 +0200

    Once ap upon a time... (read the READMEinside Div&Hero ;))

---
+++ Div&Hero/README.md
@@ -0,0 +1,12 @@
+Map Generator - "Mapa 1.0"
+Engin - MasterKaczyNoris
+Frontend - Jokson100prc
+
+Once upon a time, when Jok sat next week in front of his computer screen to understand HTML...
+next to his window stoped the Master PHP. Curious with a man under the moss, he decided to check what The MossMan is looking at.
+
+- What are you looking at, son? (althoughThe MossMan could then look like the grandfather of all grandparents) - he asked...
+
+- This strange... HTML ... I do not understand anything... -Jokson said as if to himself.
+
+MasterKaczyNoris looked at the JS code and pulled the herb wrapped in noble paper ... - Jokson, Jokson... - he said...
---
+++ Div&Hero/script.js
@@ -0,0 +1,158 @@
+window.onload = function (e) {
+    const main = document.getElementById("main");
+    const ui = document.getElementById("ui");
+    const createObject = function () {
+        var div = document.createElement('div');
+        div.innerHTML = "div";
+        return div;
+    };
+    var staticObjList = {
+        objectTypeList: {
+            "zamek": {
+                1: "zamek-1",
+                2: "zamek-2",
+                3: "zamek-3",
+            },
+            "surowce": {
+                1: "surowce-1",
+                2: "surowce-2",
+                3: "surowce-3",
+            },
+            "droga": {
+                1: "droga-1",
+                2: "droga-2",
+                3: "droga-3",
+            },
+            "woda": {
+                1: "woda-1",
+                2: "woda-2",
+                3: "woda-3",
+            },
+            "przeszkoda": {
+                1: "przeszkoda-1",
+                2: "przeszkoda-2",
+                3: "przeszkoda-3",
+            },
+
+            "odleglosc": {
+                1: "odleglosc-1",
+                2: "odleglosc-2",
+                3: "odleglosc-3",
+            },
+        },
+        getBoost: function (boost, id) {
+            console.log([boost, id]);
+            var arr = [];
+            for (var i = 0; i < boost; i++) {
+                arr.push(id);
+            }
+            return arr;
+        },
+        generate: function (collection) {
+            return collection[
+                Math.floor(
+                    Math.random() * (
+                        Object.keys(collection).length - 1 + 1
+                    )
+                ) + 1
+            ];
+        },
+        1: {
+            obj: createObject(),
+            boost: 2,
+            generateType: function () {
+                this.obj.classList.add(
+                    staticObjList.generate(
+                        staticObjList.objectTypeList["zamek"]
+                    )
+                );
+                outerHtml = this.obj.outerHTML;
+                this.obj = createObject();
+                return outerHtml;
+            }
+        },
+        2: {
+            obj: createObject(),
+            boost: 100,
+            generateType: function () {
+                this.obj.classList.add(
+                    staticObjList.generate(
+                        staticObjList.objectTypeList["surowce"]
+                    )
+                );
+                outerHtml = this.obj.outerHTML;
+                this.obj = createObject();
+                return outerHtml;
+            }
+        },
+        3: {
+            obj: createObject(),
+            boost: 100,
+            generateType: function () {
+                this.obj.classList.add(
+                    staticObjList.generate(
+                        staticObjList.objectTypeList["droga"]
+                    )
+                );
+                outerHtml = this.obj.outerHTML;
+                this.obj = createObject();
+                return outerHtml;
+            }
+        },
+        4: {
+            obj: createObject(),
+            boost: 100,
+            generateType: function () {
+                this.obj.classList.add(
+                    staticObjList.generate(
+                        staticObjList.objectTypeList["odleglosc"]
+                    )
+                );
+                outerHtml = this.obj.outerHTML;
+                this.obj = createObject();
+                return outerHtml;
+            }
+        },
+
+        // 2: "<div data-static='surowce'>0</div>",
+        // 3: "<div data-static='droga'>0</div>",
+        // 4: "<div data-static='woda'>0</div>",
+        // 5: "<div data-static='przeszkoda'>0</div>",
+        // 6: "<div>&nbsp;&nbsp;&nbsp;&nbsp;1</div>",
+        // 7: "<div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1</div>",
+        // 8: "<div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1</div>",
+    };
+    main.innerHTML = "Mapa 1.0";
+
+    ui.onclick = function () {
+        staticObjList[1].boost = document.getElementById("zamek").value;
+        staticObjList[2].boost = document.getElementById("surowce").value;
+        staticObjList[3].boost = document.getElementById("droga").value;
+        staticObjList[4].boost = document.getElementById("odleglosc").value;
+        i = document.getElementById("ilosc").value;
+        let initCollection = [1, 2, 3, 4];
+        let idCollection = [1, 2, 3, 4];
+        for (id of initCollection) {
+            console.log(staticObjList.getBoost(staticObjList[id].boost, id));
+            idCollection = idCollection.concat(staticObjList.getBoost(staticObjList[id].boost, id));
+        }
+        console.log(idCollection);
+
+        n = 4;
+        let space = "";
+
+
+        while (i--) {
+
+            id = idCollection[Math.floor(Math.random() * idCollection.length)];
+            staticObj = staticObjList[id];
+
+
+
+
+            space =
+                space + staticObj.generateType();
+            main.innerHTML = space;
+        }
+    }
+}
