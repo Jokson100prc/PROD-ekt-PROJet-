57a5d69d5ad6c06896b3a38204ce47b3c80fb154
Jokson100prc <marcinzacki@gmail.com>
2019-05-11 11:24:26 +0200

    faild

--- []KursWebDev/moduł 4z6 React JS/section_2/exercise01_add-elements/Sentences.js
+++ []KursWebDev/moduł 4z6 React JS/section_2/exercise01_add-elements/Sentences.js
@@ -1,54 +1,92 @@
-class Sentence extends React.Component {
+class App extends React.Component {
   state = {
-    textOptions: ["1", "2", "3"],
-    option: null,
-    value: ""
+    texts: ["X", "Y", "Z"],
+    txt: "*"
   };
 
-  handleRandomText = () => {
-    const index = Math.floor(Math.random() * this.state.textOptions.length);
+  handleText = () => {
+    const index = Math.floor(Math.random() * this.state.texts.length);
     this.setState({
-      option: this.state.textOptions[index]
+      txt: this.state.texts[index]
     });
   };
 
-  handleInputValue = e => {
+  setTxtFromTexts = e => {
+    // e.preventDefault();
+    if (e.target.value === "") {
+      return alert("Co do jasnej ciasnej?!");
+    }
+    const texts = [...this.state.texts];
     this.setState({
-      value: e.target.value
+      texts: texts.concat[e.target.value]
     });
-  };
-
-  addTextOptions = () => {
-    if (this.state.value === "") return alert("write something in the field!");
-    // const textOptions = [...this.state.textOptions];
-    // textOptions.push(this.state.value);
-    const textOptions = this.state.textOptions.concat(this.state.value);
-
-    this.setState({
-      textOptions, // textOptions:textOptions,
-      value: ""
-    });
-    return alert(`Array contains: ${textOptions}`);
   };
 
   render() {
     return (
       <>
-        <label>
-          Add sentence &nbsp;
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleInputValue.bind(this)}
-          />
-        </label>
-        <button onClick={this.addTextOptions}>&nbsp; Add &nbsp;</button>
-        <br />
-        <br />
-        <button onClick={this.handleRandomText}>Drow</button>
-        {this.state.option ? <h2>{this.state.option}</h2> : null}
+        <input type="text" value={this.state.value} />
+        <button onClick={this.setTxtFromTexts}>Add</button>
+
+        <button onClick={this.handleText}>Txt</button>
+        <h2>{this.state.txt}</h2>
       </>
     );
   }
 }
-ReactDOM.render(<Sentence />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// class Sentence extends React.Component {
+//   state = {
+//     textOptions: ["1", "2", "3"],
+//     option: null,
+//     value: ""
+//   };
+
+//   handleRandomText = () => {
+//     const index = Math.floor(Math.random() * this.state.textOptions.length);
+//     this.setState({
+//       option: this.state.textOptions[index]
+//     });
+//   };
+
+//   handleInputValue = e => {
+//     this.setState({
+//       value: e.target.value
+//     });
+//   };
+
+//   addTextOptions = () => {
+//     if (this.state.value === "") return alert("write something in the field!");
+//     // const textOptions = [...this.state.textOptions];
+//     // textOptions.push(this.state.value);
+//     const textOptions = this.state.textOptions.concat(this.state.value);
+
+//     this.setState({
+//       textOptions, // textOptions:textOptions,
+//       value: ""
+//     });
+//     return alert(`Array contains: ${textOptions}`);
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <label>
+//           Add sentence &nbsp;
+//           <input
+//             type="text"
+//             value={this.state.value}
+//             onChange={this.handleInputValue.bind(this)}
+//           />
+//         </label>
+//         <button onClick={this.addTextOptions}>&nbsp; Add &nbsp;</button>
+//         <br />
+//         <br />
+//         <button onClick={this.handleRandomText}>Drow</button>
+//         {this.state.option ? <h2>{this.state.option}</h2> : null}
+//       </>
+//     );
+//   }
+// }
+// ReactDOM.render(<Sentence />, document.getElementById("root"));
